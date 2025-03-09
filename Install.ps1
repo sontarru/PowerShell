2a7397026ced7830441c283afb5044221f2cb107
fkthat <fkthat@yandex.ru>
2025-03-09 03:25:53 +0300

    Add installation script.

---
+++ Install.ps1
@@ -0,0 +1,89 @@
+function InstallFromPSGallery($Name) {
+    Write-Host "Installing $Name"
+    Install-PSResource $Name -Repository PSGallery -Reinstall
+}
+
+function InstallFromGitHub([string[]] $Name) {
+    foreach($n in $Name) {
+        $n = "FkThat.PowerShell.$n"
+        Write-Host "Installing $n"
+        Install-PSResource $n -Repository GitHub `
+            -Reinstall -WarningAction SilentlyContinue
+    }
+}
+
+InstallFromGitHub "Profile"
+
+if(Get-Command git -ErrorAction SilentlyContinue) {
+    InstallFromPSGallery "Posh-Git"
+    InstallFromGitHub "Git"
+}
+
+$docker = $IsWindows ?
+    "$env:ProgramFiles\Docker\Docker\resources\bin\docker.exe" :
+    (which docker)
+
+if(Test-Path $docker) {
+    InstallFromPSGallery "DockerCompletion"
+}
+
+$rgen = $IsWindows ?
+    "$env:USERPROFILE\.dotnet\tools\reportgenerator.exe" :
+    (which reportgenerator)
+
+if(Test-Path $rgen) {
+    InstallFromGitHub 'CodeCoverage'
+}
+
+InstallFromGitHub "PasswordGen"
+
+if($IsWindows) {
+    if(Test-Path "$env:LOCALAPPDATA\Microsoft\WindowsApps\winget.exe") {
+        InstallFromGitHub 'WinGet'
+    }
+
+    InstallFromGitHub 'Aliases', 'Environment', 'PowerPlan'
+}
+
+#
+# Capitalize modules
+#
+
+$moduleRoot = $IsWindows ?
+    "$env:USERPROFILE\Documents\PowerShell\Modules" :
+    "$env:HOME/.local/share/powershell/Modules"
+
+foreach($moduleDir in (Get-ChildItem $moduleRoot -Filter "FkThat.PowerShell.*")) {
+    foreach($moduleVersionDir in (Get-ChildItem $moduleDir)) {
+        $modulePsdFile = Get-ChildItem $moduleVersionDir -Filter "$($moduleDir.Name).psd1" |
+            Select-Object -First 1
+
+        if(-not $modulePsdFile) {
+            continue
+        }
+
+        $moduleXmlFile = Get-Item (Join-Path $moduleVersionDir.FullName "PSGetModuleInfo.xml") `
+            -ErrorAction SilentlyContinue
+
+        if(-not $moduleXmlFile) {
+            continue
+        }
+
+        $moduleName = $modulePsdFile.BaseName
+
+        $moduleXml = [xml](Get-Content $moduleXmlFile)
+
+        foreach($node in $moduleXml.Objs.Obj.MS.S) {
+            if($node.N -eq 'Name') {
+                $node.InnerText = $moduleName
+                break
+            }
+        }
+
+        $moduleXml.Save($moduleXmlFile.FullName)
+
+        if($moduleDir.Name -cne $moduleName) {
+            Rename-Item $moduleDir -NewName $moduleName
+        }
+    }
+}
