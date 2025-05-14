1892c92ef35d9a383877b789a7a2b173485331e8
fkthat <fkthat@yandex.ru>
2025-05-14 09:25:55 +0300

    Add the Alias module

---
+++ src/ScaroPro.PowerShell.Alias/ScaroPro.PowerShell.Alias.psd1
@@ -0,0 +1,18 @@
+@{
+RootModule = 'ScaroPro.PowerShell.Alias.psm1'
+ModuleVersion = '1.0.0'
+GUID = 'e191ebe7-fc7c-4dea-a96a-55e9abe389ee'
+Author = 'Scaro.Pro'
+CompanyName = 'Scaro.Pro'
+Copyright = '(c) Scaro.Pro, 2025'
+Description = 'The Alias module.'
+NestedModules = @()
+FunctionsToExport = @()
+CmdletsToExport = @()
+AliasesToExport = '7z', 'gsudo', 'lua', 'less', 'vbman', 'tar', 'nvim', 'gdiff', 'ssh', 'psxp', 'scp', 
+               'ffmpeg'
+PrivateData = @{
+    PSData = @{
+    } 
+} 
+}
---
+++ src/ScaroPro.PowerShell.Alias/ScaroPro.PowerShell.Alias.psm1
@@ -0,0 +1,22 @@
+$ErrorActionPreference = "Stop"
+
+if(-not $IsWindows) {
+    Write-Error "The '$($PSVersionTable.Platform)' platform is not supported."
+}
+
+@{
+    '7z'    = "$env:ProgramFiles\7-zip\7z.exe"
+    ffmpeg  = "$env:LOCALAPPDATA\Microsoft\WinGet\Links\ffmpeg.exe"
+    gdiff   = "$env:ProgramFiles\Git\usr\bin\diff.exe"
+    gsudo   = "$env:ProgramFiles\gsudo\Current\gsudo.exe"
+    less    = "$env:ProgramFiles\Git\usr\bin\less.exe"
+    lua     = "$env:LOCALAPPDATA\Programs\Lua\bin\lua.exe"
+    nvim    = "$env:ProgramFiles\Neovim\bin\nvim.exe"
+    psxp    = "$env:LOCALAPPDATA\Microsoft\WinGet\Links\procexp64.exe"
+    scp     = "$env:SystemRoot\System32\OpenSSH\scp.exe"
+    ssh     = "$env:SystemRoot\System32\OpenSSH\ssh.exe"
+    tar     = "$env:ProgramFiles\Git\usr\bin\tar.exe"
+    vbman   = "$env:ProgramFiles\Oracle\VirtualBox\VBoxManage.exe"
+}.GetEnumerator() | ForEach-Object {
+    Set-Alias $_.Key $_.Value
+}
