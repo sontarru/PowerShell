30ec50212860f992f50085c8b91eade331e4b475
sontar <dev@sontar.ru>
2025-12-22 22:03:20 +0300

    Add PowerPlan module

---
+++ src/Sontar.PowerShell.PowerPlan/Sontar.PowerShell.PowerPlan.psd1
@@ -0,0 +1,18 @@
+@{
+RootModule = 'Sontar.PowerShell.PowerPlan.psm1'
+ModuleVersion = '0.0.1'
+GUID = '27e5e6bb-d875-4c3b-8f4c-de4e04d7fbeb'
+Author = 'Sontar'
+CompanyName = 'Sontar'
+Copyright = '(c) Sontar, 2025'
+Description = 'Windows power plan switcher.'
+NestedModules = @()
+FunctionsToExport = 'Get-PowerPlan', 'Switch-PowerPlan'
+CmdletsToExport = @()
+VariablesToExport = @()
+AliasesToExport = 'swpwp', 'gpwp'
+PrivateData = @{
+    PSData = @{
+    } # End of PSData hashtable
+} # End of PrivateData hashtable
+}
---
+++ src/Sontar.PowerShell.PowerPlan/Sontar.PowerShell.PowerPlan.psm1
@@ -0,0 +1,79 @@
+using namespace System.Management.Automation
+
+$ErrorActionPreference = "Stop"
+
+<#
+.SYNOPSIS
+Gets the list of Windows power plans.
+#>
+function Get-PowerPlan {
+    param (
+        [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [SupportsWildcards()]
+        [ArgumentCompleter({
+            Get-PowerPlan |
+                Where-Object Name -Like "$($args[2])*" |
+                Select-Object -ExpandProperty Name |
+                ForEach-Object { $_.Contains(' ') ? "'$_'" : $_ }
+        })]
+        [string[]]
+        $Name = '*'
+    )
+
+    begin {
+        $all = powercfg /l | Select-Object -Skip 2 |
+            ForEach-Object {
+                if($_ -match ':\s*(\S+)\s+\(([^\)]+)\)\s*(\*)?') {
+                    [pscustomobject]@{
+                        Id = $Matches[1]
+                        Name = $Matches[2]
+                        Active = [bool]$Matches[3]
+                    }
+                }
+            }
+
+        $filtered = @{}
+    }
+
+    process {
+        $Name | ForEach-Object {
+            $all | Where-Object Name -Like $_ |
+                ForEach-Object { $filtered[$_.Id] = $_ }
+        }
+    }
+
+    end {
+        $filtered.Values | Sort-Object Name
+    }
+}
+
+Class PowerPlanValidateSetGenerator: IValidateSetValuesGenerator {
+    [string[]] GetValidValues() {
+        return (Get-PowerPlan | Select-Object -ExpandProperty Name)
+    }
+}
+
+<#
+.SYNOPSIS
+Activates a specified Windows power plan.
+#>
+function Switch-PowerPlan {
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [string]
+        [ValidateSet([PowerPlanValidateSetGenerator])]
+        $Name
+    )
+
+    Assert-Windows
+
+    Get-PowerPlan |
+        Where-Object Name -Eq $Name |
+        Select-Object -ExpandProperty Id |
+        ForEach-Object {
+            powercfg /s $_
+        }
+}
+
+Set-Alias gpwp Get-PowerPlan
+Set-Alias swpwp Switch-PowerPlan
