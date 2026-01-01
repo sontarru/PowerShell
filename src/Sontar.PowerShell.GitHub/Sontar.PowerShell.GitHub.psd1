2909895047c4160fe4a813f3976b026e9189d130
sontar <dev@sontar.ru>
2026-01-01 05:51:18 +0300

    Add GitHub module

---
+++ src/Sontar.PowerShell.GitHub/Sontar.PowerShell.GitHub.psd1
@@ -0,0 +1,18 @@
+@{
+RootModule = 'Sontar.PowerShell.GitHub.psm1'
+ModuleVersion = '0.0.1'
+GUID = '48529026-53b8-4d78-9747-2fcc0c86bb8c'
+Author = 'Sontar'
+CompanyName = 'Sontar'
+Copyright = '(c) Sontar, 2025'
+Description = 'GitHub tools.'
+NestedModules = @()
+FunctionsToExport = 'Get-GitHubPackages'
+CmdletsToExport = @()
+VariablesToExport = @()
+AliasesToExport = @()
+PrivateData = @{
+    PSData = @{
+    } # End of PSData hashtable
+} # End of PrivateData hashtable
+}
---
+++ src/Sontar.PowerShell.GitHub/Sontar.PowerShell.GitHub.psm1
@@ -0,0 +1,52 @@
+$ErrorActionPreference = 'Stop'
+
+<#
+Gets a list of packages from the user account.
+#>
+function Get-GitHubPackages {
+    [CmdletBinding()]
+    param (
+        # Package name.
+        [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [SupportsWildcards()]
+        [string[]]
+        $Name = '*',
+
+        # Package type.
+        [Parameter()]
+        [ValidateSet("NuGet", "Container")]
+        [string[]]
+        $PackageType = @("NuGet", "Container")
+    )
+
+    begin {
+        $names = @()
+    }
+
+    process {
+        $names += $Name
+    }
+
+    end {
+        $all = @{}
+
+        $PackageType | ForEach-Object { $_.ToLower() } |
+            Select-Object -Unique |
+            ForEach-Object { gh api "/user/packages?package_type=$($_)" } |
+            ConvertFrom-Json |
+            ForEach-Object {
+                $all[$_.id] = [PSCustomObject]@{
+                    Id = $_.id
+                    PacksageType = $_.package_type
+                    Name = $_.name
+                    Visibility = $_.visibility
+                }
+            }
+
+        $all.Values | Where-Object {
+                $n = $_.Name
+                $names | Where-Object { $n -Like $_ }
+            } |
+            Sort-Object PackageType, Name
+    }
+}
