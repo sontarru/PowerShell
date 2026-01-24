d57af20af898fc5a09f8c9f86ea25d7373e7034c
sontarru <dev@sontar.ru>
2026-01-24 17:52:23 +0300

    Fix new Visual Studio behavior when dotnet is not in $PATH

--- src/Sontar.PowerShell.VisualStudio/Sontar.PowerShell.VisualStudio.psm1
+++ src/Sontar.PowerShell.VisualStudio/Sontar.PowerShell.VisualStudio.psm1
@@ -1,14 +1,23 @@
 $ErrorActionPreference = "Stop"
 
+<#
+Starts Visual Studio and open the specified solution file(s).
+#>
 function Start-VisualStudio {
     [CmdletBinding()]
     param (
+        # Path to look for solution files.
         [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [string[]]
         $Path = '.',
 
+        # Lookup subdirectories recursively.
         [switch]
-        $Recurse
+        $Recurse,
+
+        # Open Visual Studio without any solution even if no one was found.
+        [switch]
+        $Force
     )
 
     begin {
@@ -18,11 +27,12 @@ function Start-VisualStudio {
     process {
         $Path | Get-Item -ErrorAction SilentlyContinue |
             ForEach-Object {
-                if($_ -is [System.IO.FileInfo] -and $_.Extension -eq '.sln') {
+                if($_ -is [System.IO.FileInfo] -and $_.Extension -in '.sln','.slnx') {
                     $_
                 }
                 if($_ -is [System.IO.DirectoryInfo]) {
                     Get-ChildItem $_ -File -Filter '*.sln' -Recurse:$Recurse
+                    Get-ChildItem $_ -File -Filter '*.slnx' -Recurse:$Recurse
                 }
             } |
             ForEach-Object {
@@ -31,8 +41,19 @@ function Start-VisualStudio {
     }
 
     end {
-        $paths.Keys | ForEach-Object {
-            vs $_
+        $StartProcessArgs = @{
+            FilePath = "devenv.exe"
+            Environment  = @{ PATH = "$env:ProgramFiles\dotnet" }
+        }
+
+        if($paths.Keys.Count) {
+            $paths.Keys | ForEach-Object {
+                $StartProcessArgs.ArgumentList = @($_)
+                Start-Process @StartProcessArgs
+            }
+        }
+        elseif ($Force) {
+            Start-Process @StartProcessArgs
         }
     }
 }
